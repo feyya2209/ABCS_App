465fe241245f785ca27d9dd49a3fd1ecff296777
feyya2209 <imansafeyya@gmail.com>
2026-05-08 12:38:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -472,7 +472,7 @@ elif choice == "Data Augmentation":
     with col2:
         # 🚦 THE ROUTER: This points the code to the exact correct folders!
         if "Dog" in species_choice:
-            breeds = ['Afghan', 'African wild dog', 'Airedale', 'Basenji', 'Beagle'] # (Keep your full list of 70 here!)
+            breeds = ['Afghan', 'African wild dog','Airedale','American Hairless','American Spaniel','Basenji','Basset','Beagle','Bearded Collie','Bermaise','Bichon Frise','Blenheim','Bloodhound','Bluetick','Border Collie','Borzoi','Boston Terrier','Boxer','Bull Mastiff','Bull Terrier','Bulldog','Cairn','Chihuahua','Chinese Crested','Chow','Clumber','Cockapoo','Cocker','Collie','Corgi','Coyote','Dalmation','Dhole','Dingo','Doberman','Elk Hound','French Bulldog','German Sheperd','Golden Retriever','Great Dane','Great Perenees','Greyhound','Groenendael','Irish Spaniel','Irish Wolfhound','Japanese Spaniel','Komondor','Labradoodle','Labrador','Lhasa','Malinois','Maltese','Mex Hairless','Newfoundland','Pekinese','Pit Bull','Pomeranian','Poodle','Pug','Rhodesian','Rottweiler','Saint Bernard','Schnauzer','Scotch Terrier','Shar_Pei', 'Shiba Inu','Shih-Tzu', 'Siberian husky','Vizsla','Yorkie'] 
             animal_folder = "dataset_dogs"
             combined_folder = "combined_dogs"
             
